a87a535053800086b289b3a0f02237bc72848440
Ehket <ehketoy@gmail.com>
2024-06-14 13:34:53 +0300

    Style classes added

--- mailabl-qgis/Functions/EVEL/Qgis stiilid/EvelStyles.py
+++ mailabl-qgis/Functions/EVEL/Qgis stiilid/EvelStyles.py
@@ -4,3 +4,57 @@ class EVEL_ElekterSide:
 
 class Hylsid:
     hülsid = "Hülsid.qml"
+
+class Kanalisatsioon:
+    kanalikaevud_mahajaetud = "Kanalikaevud_mahajaetud.qml"
+    kanalikaevud = "Kanalikaevud.qml"
+    kanalisatsiooni_siibrid = "Kanalisatsiooni_siibrid.qml"
+    kanalisatsiooniehitised = "Kanalisatsiooniehitised.qml"
+    kanalisatsioonitorud_1_2000 = "Kanalisatsioonitorud_1_2000.qml"
+    kanalisatsioonitorud = "Kanalisatsioonitorud.qml"
+    liitmikud_kanal = "Liitmikud_kanal.qml"
+    mahajäetud_kan_torud = "Mahajäetud_kan_torud.qml"
+    pumplad = "Pumplad.qml"
+    sõlmed = "Sõlmed.qml"
+    spetsiaalkaevud = "Spetsiaalkaevud.qml"
+
+class Kliendid:
+    arvestid = "Arvestid.qml"
+    kliendid = "Kliendid.qml"
+    liitumispunktid = "Liitumispunktid.qml"
+
+class Rennid:
+    rennid = "Rennid.qml"
+
+class Servituudid:
+    servituudid = "Servituudid.qml"
+
+class Teostusjoonis:
+    teostusjoonis = "Teostusjoonis.qml"
+
+class Veevõrk:
+    hüdrandid = "Hüdrandid.qml"
+    liitmikud = "Liitmikud.qml"
+    mahajäetud_veetorud = "Mahajäetud_veetorud.qml"
+    puurkaevud = "Puurkaevud.qml"
+    sulgeseadmed = "Sulgeseadmed.qml"
+    veekaevud_mahajäetud = "Veekaevud_mahajäetud.qml"
+    veekaevud = "Veekaevud.qml"
+    veetöötlusjaamad = "Veetöötlusjaamad.qml"
+    veetornid = "Veetornid.qml"
+    veetorud_1_2000 = Veetorud_1_2000.qml
+    veetorud = "Veetorud.qml"
+    veevõrgupumplad = "Veevõrgupumplad.qml"
+
+class Võrgusündmused_Autod:
+    autod = "Autod.qml"
+    avarii_ja_hooldustööd = "Avarii_ja_hooldustööd.qml"
+    
+
+
+
+
+
+
+
+
